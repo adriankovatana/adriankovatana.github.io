a26db066d34883f53565d7e18ae242d343fca926
Adrian Kovatana <adrian.kovatana@gmail.com>
2017-12-01 11:57:58 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# adriank1.github.io
+Portfolio website
+
+Visit the page [here](https://adriank1.github.io/index.html)
---
+++ apps/expand_content_animate_iframe_v2/main.js
@@ -0,0 +1,53 @@
+function initContent() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      setContent(this);
+    }
+  };
+  xhttp.open("GET", "content.xml", true);
+  xhttp.send();
+}
+
+function setContent(xml) {
+  var xmlDoc = xml.responseXML;
+  var section = xmlDoc.getElementsByTagName("section");
+  var content = '';
+
+  for(var i = 0; i < section.length; i++) {
+    var contentElement = section[i]
+      .getElementsByTagName("content");
+    var paragraphs = contentElement[0]
+      .getElementsByTagName("paragraph");
+    for(var j = 0; j < paragraphs.length; j++) {
+      content += "<p>" + paragraphs[j]
+        .childNodes[0]
+        .nodeValue + "</p>";
+    }
+
+    var divName = "hiddenModalContent" + i.toString();
+    document.getElementById(divName).innerHTML = content;
+    document.getElementById(divName).setAttribute("style", "opacity: 0.0;")
+
+    //Reset content to empty
+    content = '';
+  }
+}
+
+function toggleHideDiv(indexNumber) {
+  var divName = "hiddenModal" + indexNumber.toString();
+  var div = document.getElementById(divName);
+
+  var textDivName = "hiddenModalContent" + indexNumber.toString();
+  var textDiv = document.getElementById(textDivName);
+
+  if(div.style.display === 'none'){
+    $(div).slideDown(300, function() {
+      $(textDiv).fadeTo(500, 1);
+    });
+  } else {
+    $(textDiv).fadeTo(500, 0, function() {
+      $(div).slideUp(300);
+    });
+  }
+}
